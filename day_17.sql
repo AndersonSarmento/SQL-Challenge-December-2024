05068c1dd0fb8f52e15f2f93319b7f03ab772764
Anderson Sarmento <anderson.sarmento@gmail.com>
2024-12-17 21:52:26 -0300

    Day 17 of SQL Advent Calendar

--- day_17.sql
+++ day_17.sql
@@ -15,3 +15,7 @@ Question level of difficulty:
 Easy
 GrinchGray GrinchGray Grinch
 =========================================================================================
+SELECT prank_name, location
+FROM grinch_pranks
+WHERE difficulty = 'Advanced' OR difficulty = 'Expert'
+ORDER BY prank_name DESC, location DESC;
