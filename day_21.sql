75bbaf6e72ee203a285589be64edb0b7e350b553
Anderson Sarmento <anderson.sarmento@gmail.com>
2024-12-21 23:08:26 -0300

    Day 21 of SQL Advent Calendar

--- day_21.sql
+++ day_21.sql
@@ -14,3 +14,12 @@ gift_id	gift_name	recipient_type	weight_kg
 Question level of difficulty:
 Hard
 GrinchGrinchGrinch
+===============================================================================================================
+SELECT
+    recipient_type,
+    SUM(weight_kg) AS total_weight,
+    (SUM(weight_kg) * 100.0 / (SELECT SUM(weight_kg) FROM gifts)) AS weight_percentage
+FROM
+    gifts
+GROUP BY
+    recipient_type;
